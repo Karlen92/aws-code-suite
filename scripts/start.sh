68f77a6223b56ef1be49edc27dbba314c3c66b3a
Karlen92 <mkkarlen92@gmail.com>
2019-10-10 18:51:06 +0400

    start script changes

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-export PM2_HOME=/home/ec2-user/.pm2
-pm2 delete prod
-cd /home/ec2/aws-code-suite/
-pm2 start 'npm start' --name prod
+cd /home/ec2-user/aws-code-suite
+npm run prod
