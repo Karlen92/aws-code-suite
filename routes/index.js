e78a41a320211e92bf2cc63f08a0d70d77b5583b
Karlen92 <mkkarlen92@gmail.com>
2019-10-11 12:10:25 +0400

    start script changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.send('version: 1.0.3');
+  res.send('version: 1.0.4');
 });
 
 module.exports = router;
